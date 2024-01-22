681d04ac2d7f13a0eeccd7d1326f8885169d621f
flaviostutz <flaviostutz@gmail.com>
2024-01-22 23:27:30 +0100

    chore: distribute types in lib

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * as devportalV1 from './v1/generated/types/devportal';
 export * as publisherV1 from './v1/generated/types/publisher';
 export * from './client';
 export * from './utils';
+export * from './types';
